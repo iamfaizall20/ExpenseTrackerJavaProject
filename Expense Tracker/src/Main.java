09c77fbdf55e6d029ea00d0744128dfc27ad8356
Danish Ali <danishalikango1@gmail.com>
2025-04-16 22:19:59 +0500

    Name Comment

--- Expense Tracker/src/Main.java
+++ Expense Tracker/src/Main.java
@@ -23,7 +23,7 @@ public class Main {
     public static final String PURPLECOLOR = "\033[35m";
 
     Expense[] expenselist = new Expense[150];
-
+    //Danish ALI kANGO
     static Scanner getInput = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Pull Requested");
